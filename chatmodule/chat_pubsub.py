240f64c266b85f6d38e74f13e56d04d1d1e565b0
vishveshsoni <vishveshsoni54@gmail.com>
2020-07-26 05:35:21 +0530

    added char, data processing module

---
+++ chatmodule/chat_pubsub.py
@@ -0,0 +1,49 @@
+from google.cloud import pubsub_v1
+import os
+
+os.environ[
+    "GOOGLE_APPLICATION_CREDENTIALS"] = "/home/vishvesh/Documents/Dal/serverless/api-8566414966874230052-395627-4fca061a25a4.json"
+
+project_id = "api-8566414966874230052-395627"
+topic_id = "groupchat"
+sub_id = "sub_grpchat"
+# PUBLISHER
+def publish():
+
+    publisher = pubsub_v1.PublisherClient()
+    topic_name = 'projects/{project_id}/topics/{topic}'.format(
+        project_id=project_id,
+        topic=topic_id,  # Set this to something appropriate.
+    )
+    # publisher.create_topic(topic_name)
+
+    publisher.publish(topic_name, b'My first message!')
+
+
+# SUBSCRIBER
+def subscribe():
+    subscriber = pubsub_v1.SubscriberClient()
+    topic_name = 'projects/{project_id}/topics/{topic}'.format(
+        project_id=project_id,
+        topic=topic_id,
+    )
+    subscription_name = 'projects/{project_id}/subscriptions/{sub}'.format(
+        project_id=project_id,
+        sub=sub_id,
+    )
+    # subscriber.create_subscription(
+    #     name=subscription_name, topic=topic_name)
+
+    def callback(message):
+        print(message.data)
+        message.ack()
+
+    future = subscriber.subscribe(subscription_name, callback)
+    try:
+        future.result()
+    except KeyboardInterrupt:
+        future.cancel()
+
+if __name__ == "__main__":
+    # publish()
+    subscribe()
